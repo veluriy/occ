d72d68bdc3d7a8f5bed305aac1318590160b8575
ptlx <ptlx@outlook.jp>
2023-09-18 00:53:23 +0900

    useの変更

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::env;
+use std::{env, process};
 
 fn main(){
     let args: Vec<String> = env::args().collect();
     // println!("{:?}", args);
     if args.len() != 2 {
         eprintln!("引数の数が異なります");
-        std::process::exit(1);
+        process::exit(1);
     }
     println!(".intel_syntax noprefix");
     println!(".globl main");
