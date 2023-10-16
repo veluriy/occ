1d4e1e875e1c449b9985274da7bff6834f81e4ac
ok <ptlx@outlook.jp>
2023-10-16 05:42:41 +0000

    空白文字が入力の場合に対応

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use occ::generate_assembly;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("引数の数が異なります");
-        process::exit(1);
-    }
-    generate_assembly(&args[1]);
+  let mut args: Vec<String> = env::args().collect();
+  if args.len() != 2 {
+    eprintln!("引数の数が異なります");
+    process::exit(1);
+  }
+  // args[1]から空白を削除してそれをgenerate_assembly関数に渡したい
+  args[1].retain(|c| !c.is_whitespace());
+  //
+  generate_assembly(&args[1]);
 }
