b6ccfe5d771fa987bf2375da489529f8f2236d06
ptlx <ptlx@outlook.jp>
2023-10-18 00:25:25 +0900

    余分なコメントを削除

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,5 @@ fn main() {
         eprintln!("引数の数が異なります");
         process::exit(1);
     }
-    // args[1]から空白を削除してそれをgenerate_assembly関数に渡したい
-    //args[1].retain(|c| !c.is_whitespace());
-    //
     generate_assembly(&args[1]);
 }
